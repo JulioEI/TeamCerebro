3a4ff53d8b4c249686da201f5b114021c551641a
JulioEI <48024498+JulioEI@users.noreply.github.com>
2021-04-13 16:56:26 +0200

    Add files via upload

--- evaluate_methods.m
+++ evaluate_methods.m
@@ -55,3 +55,42 @@ for iter = 1:length(iters)
     end
 end
 
+
+group_label = [{'CNB+Mean'}, {'CNB+Kalman'}, {'dSVM+Mean'}, {'dSVM+Kalman'}, {'gSVM+Mean'},{'gSVM+Kalman'}];
+groups = repmat(group_label, 20, 1);
+
+groups = groups(:);
+X = [tim(:),kfold(:)];
+
+figure 
+gscatter(X(:,1),X(:,2),groups); 
+h = gca; 
+cxlim = h.XLim; 
+cylim = h.YLim; 
+hold on 
+
+Mu = zeros(6,2); % Extract the means 
+Sigma = zeros(2,2,6); 
+col = 0.5*[1,0,0; 1,1,0;0,1,0;0,1,1;0,0,1;1,0,1];
+txt_pos = [4.5,13;7,28;1,12;4,27;19,10;20,26];
+for j = 1:6     
+    Mu(j,1) = mean(tim(:,j));
+    Mu(j,2) = mean(kfold(:,j));
+    
+    Sigma(:,:,j) = cov(tim(:,j), kfold(:,j))^2;
+    xlim = Mu(j,1) + 4*[-1 1]*sqrt(Sigma(1,1,j));     
+    ylim = Mu(j,2) + 4*[-1 1]*sqrt(Sigma(2,2,j));     
+    f = @(x1,x2)reshape(mvnpdf([x1(:),x2(:)],Mu(j,:),Sigma(:,:,j)),size(x1));     
+    fcontour(f,[xlim ylim],'--','LineColor', col(j,:)) % Draw contours for the multivariate normal distributions  
+    text(txt_pos(j,1),txt_pos(j,2),group_label{j},'FontSize',14,'Color', col(j,:))
+end
+
+h.XLim = cxlim; 
+h.YLim = cylim; 
+xlabel('Running Time (s)') 
+ylabel('RMSE') 
+legend 'off'
+set(gca,'FontSize',20)
+set(gca,'TickDir','out')
+set(gca,'Box','off')
+
